5352a01fac628e69517bf0aba2861e0cf8e41470
AO3828 <ongktph@gmail.com>
2025-08-06 19:14:54 +0800

    Update utility.py

--- helper_functions/utility.py
+++ helper_functions/utility.py
@@ -3,7 +3,7 @@ import streamlit as st
 import random  
 import hmac 
 import os
-from dotenv import load_dotenv
+
 
 # """  
 # This file contains the common components used in the Streamlit App.  
@@ -14,7 +14,7 @@ def check_password():
     """Returns `True` if the user had the correct password."""  
     def password_entered():  
         """Checks whether a password entered by the user is correct."""  
-        if hmac.compare_digest(st.session_state["password"], dotenv.["password"]):  
+        if hmac.compare_digest(st.session_state["password"], os.getenv["password"]):  
             st.session_state["password_correct"] = True  
             del st.session_state["password"]  # Don't store the password.  
         else:  
